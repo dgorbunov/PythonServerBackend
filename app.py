03580f281eb404392c69f2658a58a6c3d122004b
Daniel <danny.gorbunov@gmail.com>
2020-02-08 13:01:20 -0500

    Commit 2

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-return 'Hello World!'
+@app.route('/', methods=['GET'])
+def home():
+    return 'Hello World!'
+
+app.run(port=3000)
